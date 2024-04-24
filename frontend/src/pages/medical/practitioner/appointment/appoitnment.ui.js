328bcfbecb75a0a2947ba7bcd2314680254ed2aa
xiangyuliualex <xiangyuliualex@gmail.com>
2024-04-25 00:52:47 +0100

    appointment inital version finished

--- frontend/src/pages/medical/practitioner/appointment/appoitnment.ui.js
+++ frontend/src/pages/medical/practitioner/appointment/appoitnment.ui.js
@@ -11,40 +11,129 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-export function BasicTable({ appointments, onUpdateAppointment, onRefetch }) {
-  const handleOnUpdate = async (
-    appointmentId,
-    gpId,
-    slotId,
-    gpName,
-    time,
-    date,
-    status
-  ) => {
-    await onUpdateAppointment(
-      appointmentId,
-      gpId,
-      slotId,
-      gpName,
-      time,
-      date,
-      status
-    );
-    onRefetch();
+function AlternativeComponent({appointment,onUpdateAppointment}){
+  const [open, setOpenAlt] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpenAlt(true);
   };
+  const handleClose = () => {
+    setOpenAlt(false);
+  };
+  console.log(appointment.id);
+  // gpId,slotId,gpName,time,date,status
+  const handleonUpdate=(formJson,appointment)=>{
+    console.log(appointment);
+    const updateData={};
+    updateData.id=appointment.id;
+    if(formJson.gpId!=='') updateData.gpId=formJson.gpId;
+    if(formJson.slotId!=='') updateData.slotId=formJson.slotId;
+    if(formJson.gpName!=='') updateData.gpName=formJson.gpName;
+    if(formJson.time!=='') updateData.time=formJson.time;
+    const dataToSend={...appointment, ...updateData};
+    onUpdateAppointment(dataToSend.id,dataToSend.gpId,dataToSend.slotId,dataToSend.gpName,dataToSend.time,dataToSend.date,"Approved");
+  }
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Alternative
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            // const gpId = formJson.gpId;
+            // const slotId=formJson.slotId;
+            // const gpName=formJson.gpName;
+            // const time=formJson.time;
+            // const date=formJson.date;
+            // console.log(gpId,slotId,gpName,time,date);
+            handleonUpdate(formJson,appointment);
+
+            // console.log(slotId==="");
+            handleClose();
+          },
+        }}
+        // gpId,slotId,gpName,time,date,status
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter the field you want to changed. Please leave the unChange field empty!!
+          </DialogContentText>
+          <TextField
+            margin="dense"
+            id="gpId"
+            name="gpId"
+            label="GP Id"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            id="slotId"
+            name="slotId"
+            label="Slot Id"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+           <TextField
+            margin="dense"
+            id="gpName"
+            name="gpName"
+            label="GP Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            id="time"
+            name="time"
+            label="Time"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            id="date"
+            name="date"
+            label="Date"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
+
+
+
+
+
+export function BasicTable({ appointments, onUpdateAppointment}) {
+
 
   return (
     <TableContainer component={Paper}>
@@ -72,21 +161,27 @@ export function BasicTable({ appointments, onUpdateAppointment, onRefetch }) {
               <TableCell align="right">{row.reason}</TableCell>
               <TableCell align="right">{row.status}</TableCell>
               <TableCell align="right">
-                <button
+                <Button variant="outlined"
                   onClick={() =>
-                    handleOnUpdate(
-                      row.id,
-                      row.gpId,
-                      row.slotId,
-                      row.gpName,
-                      row.time,
-                      row.date,
-                      "Approved"
-                    )
-                  }
+                    onUpdateAppointment(row.id,row.gpId,row.slotId,row.gpName,row.time,row.date, "Approved")}
                 >
                   Accept
-                </button>
+                </Button>
+              </TableCell>
+              <TableCell align="right">
+                <Button variant="outlined"
+                  onClick={() =>
+                    onUpdateAppointment(row.id,row.gpId,row.slotId,row.gpName,row.time,row.date, "Rejected")}
+                >
+                  Reject
+                </Button>
+              </TableCell>
+              <TableCell align="right">
+                <AlternativeComponent 
+                                      appointment={row}
+                                      onUpdateAppointment={onUpdateAppointment}
+
+                />
               </TableCell>
             </TableRow>
           ))}
@@ -97,15 +192,9 @@ export function BasicTable({ appointments, onUpdateAppointment, onRefetch }) {
 }
 
 export function HandleAppointmentPage() {
-  const { data, isLoading, error, refetch } = useHandleAppointmentQuery(
-    sessionStore.getState().uid
-  );
-  const {
-    mutate: mutateAppointment,
-    isLoad,
-    isError,
-    isSuccess,
-  } = useUpdateAppointmentMutation();
+  const { data, isLoading, error, refetch } = useHandleAppointmentQuery(sessionStore.getState().uid);
+  const {mutate: mutateAppointment,isLoad,isError,isSuccess,} = useUpdateAppointmentMutation();
+  
 
   if (error) return <h4>Error:{error.message}, retry again</h4>;
   if (isLoading) return <h4>...Loading data</h4>;
@@ -117,15 +206,7 @@ export function HandleAppointmentPage() {
     refetch();
     // 可在这里调用需要执行的函数
   }
-  const handleUpdateAppointment = (
-    appointmentId,
-    gpId,
-    slotId,
-    gpName,
-    time,
-    date,
-    status
-  ) => {
+  const handleUpdateAppointment = (appointmentId,gpId,slotId,gpName,time,date,status) => {
     mutateAppointment({
       appointmentId: appointmentId,
       gpId: gpId,
@@ -143,7 +224,6 @@ export function HandleAppointmentPage() {
       <BasicTable
         appointments={appointments}
         onUpdateAppointment={handleUpdateAppointment}
-        onRefetch={refetch}
       />
     </div>
   );
