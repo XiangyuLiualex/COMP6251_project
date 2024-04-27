4ed6d420e97fa48d8d9b9ee7e9bbdcee1b4bd315
Chz <chzwengweng@gmail.com>
2024-04-28 00:27:34 +0100

    vite mirgration success

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,34 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
-    server: {
-        port: 3000,
-    },
-    plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        css: true,
-        reporters: ['verbose'],
-        coverage: {
-            reporter: ['text', 'json', 'html'],
-            include: ['src/**/*'],
-            exclude: [],
-        }
-    },
-    esbuild: {
-        loader: "jsx"
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            jsx: "automatic",
-            loader: {
-                ".js": "jsx",
-            },
-        },
-    },
+  server: {
+    port: 3000,
+  },
+  plugins: [react()],
 })
