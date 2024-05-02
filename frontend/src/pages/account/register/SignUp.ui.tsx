5581f5f2511aa2625b64593436c5069894de13c8
xiangyuliualex <xiangyuliualex@gmail.com>
2024-05-02 15:06:09 +0100

    information

--- frontend/src/pages/account/register/SignUp.ui.tsx
+++ frontend/src/pages/account/register/SignUp.ui.tsx
@@ -3,6 +3,68 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Send } from "@mui/icons-material";
 import { SignUpForm, useSignUpMutation } from "../../../entities/register";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+export default function InformationPermission() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Provide Consent
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Consent for Access to Medical Information"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        To ensure the highest standard of medical service and efficient care coordination, 
+                        we require your consent to share your basic personal information and medical history 
+                        with healthcare professionals involved in your treatment. All shared information will 
+                        be handled confidentially and accessed only by authorized personnel.
+                        <br />
+                        <strong>If you do not provide consent, you will not be able to use this website.</strong>
+                        This measure is necessary to maintain the integrity and security of our operations 
+                        and your medical care.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Disagree
+                    </Button>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
+
+
+
+
 
 // TODO common lib function, add default value for T
 // transform FormData to json type T
@@ -46,6 +108,7 @@ export function SignUpPage() {
                     <Typography component="h1" variant="h5">
                         Sign up
                     </Typography>
+                    <InformationPermission/>
                     <Box sx={{
                         marginTop: 4,
                         display: 'flex',
